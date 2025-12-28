479d25820aa60e903029862243fc1902b6582b6e
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-28 15:14:25 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    botName: '💎 Senior Bot MD',
+    owner: '6281234567890', // Ganti dengan nomor owner (format 62)
+    prefix: ['.', '!', '#'],
+    businessInfo: "Bisnis ini menggunakan layanan aman dari perusahaan Meta untuk mengelola chat ini. Ketuk untuk info selengkapnya.",
+    thumbnail: './lib/thumbnail.jpg', // Buat file thumbnail.jpg di folder lib
+    sessionPath: './session',
+    databasePath: './database.json'
+};
